440abb7fae234a287cf7d41f2bf021981b934dc6
Alejandro <sancha0701@gmail.com>
2022-09-04 18:54:53 -0400

    commit tree 028e8a884ca8b2e178575f07031655187cb40292

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-// Your code here
+
+function createEmployeeRecord(record) {
+    
+    const employeeRecords = {
+    firstName: record[0],
+    familyName:record[1],
+    title: record[2],
+    payPerHour: record[3],
+    timeInEvents: [],
+    timeOutEvents: []
+}
+return employeeRecords
+    
+    }
+
+    function createEmployeeRecords(employee){
+       return employee.map(employee => createEmployeeRecord(employee))
+    }
+
+    function createTimeInEvent(employee, dateStamp) {
+        let [date, hour] = dateStamp.split(" ")
+        let eventObj= {
+            type: "TimeIn",
+            hour: parseInt(hour, 10),
+            date
+
+        }
+        employee.timeInEvents.push(eventObj)
+        return employee
+            }
+
+
+      function createTimeOutEvent(employee, dateStamp)  {
+        let [date, hour] = dateStamp.split(" ")
+        let eventObj= {
+            type: "TimeOut",
+            hour: parseInt(hour, 10),
+            date
+      } 
+      employee.timeOutEvents.push(eventObj)
+      return employee
+
+     }
+    function hoursWorkedOnDate(employee, dateStamp) {
+    const timeIn = employee.timeInEvents.find(employee=> employee.date === dateStamp)
+    const timeOut = employee.timeOutEvents.find(employee => employee.date === dateStamp)
+    //console.log("timeIn", timeIn)
+
+    return (timeOut.hour - timeIn.hour)/100
+
+}
+
+    function wagesEarnedOnDate(employee, dateStamp) {
+        const hours = hoursWorkedOnDate(employee, dateStamp)
+        const payHours = employee.payPerHour*hours
+        return  parseFloat(payHours.toString())
+    }
+
+     const allWagesFor = function(employee) {
+        const eligibleDates = employee.timeInEvents.map(function (e) {
+            return e.date
+        })
+        const payable = eligibleDates.reduce((memo, record) => memo + wagesEarnedOnDate(employee, record), 0)
+        //console.log(payable) 
+        return payable
+     }
+
+     
+
+    function calculatePayroll (employeeRecords) {
+        //console.log(employeeRecords)
+        return employeeRecords.reduce((accumulator, employee) => accumulator + allWagesFor(employee),0)
+        
+    }
